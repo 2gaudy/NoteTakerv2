1a8b583f0f6e60ca310787ed751bdabb26099366
2gaudy <agrant9946@gmail.com>
2022-10-27 00:35:34 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -2,35 +2,58 @@ import './App.css';
 import React from 'react'
 
 function App() {
-
-  const [text, setText] = React.useState("Write new text here!")
+  const [title, setTitle] = React.useState("Write the title here!")
+  const [text, setText] = React.useState("Write body text here!")
   const [notes, setNotes] = React.useState([])
 
   return (
     <div className="App">
       
-      <div className="input-section">
-        <div className="title">
+      <div className="title">
           <h1>NoteTaker!</h1>
-        </div>
+      </div>
 
-        <input className="input-button" type="button" value="Save" onClick={() => {
-          setNotes([...notes, text]);
-          setText("Write something here!")
+      <div className="input-section">
+        
+        <input className="title-input" 
+          type="text" 
+          value={title} 
+          onChange={(event) => {
+            const newValue = event.target.value;
+            setTitle(newValue);
           }}
         ></input>
 
-        <input className="input-text" type="text" value={text} 
+{/* Input Text Box */}
+       
+        <input className="input-text" 
+          type="text" 
+          value={text} 
           onChange={(event) => {
             const newValue = event.target.value;
             setText(newValue);
           }}
         ></input>
+
+{/* Input Button */}
+        
+        <input className="input-button" 
+          type="button" 
+          value="Save" 
+          onClick={() => {
+            setNotes([...notes, text]);
+            setText("Write something here!")
+          }}
+        ></input>
+
       
       </div>
+      
+{/* Notes Section */}
 
       <div className='notes-section'>
-
+      
+{/* Display Notes */}
       {notes.map((note) => (
         <div className='note'>
           <p className='note-text'>{note}</p>
